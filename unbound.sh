49295e3c46567b8c6890883ac3f30624efeb18ef
Wim <11030068+wimb0@users.noreply.github.com>
2024-11-27 17:53:24 +0100

    Update unbound.sh

--- unbound.sh
+++ unbound.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/wimb0/unbound-lxc/main/build.func)
+
 # Copyright (c) 2021-2024 community-scripts ORG
 # Author: wimb0
 # License: MIT
